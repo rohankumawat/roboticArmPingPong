68f73f7b58fcb7fb8584531b633a2a7542283421
thelastsayian <123329017+thelastsayian@users.noreply.github.com>
2023-04-16 20:52:03 +0100

    Update ObjectTracking.cpp

--- Implementation/src/ObjectTracking.cpp
+++ Implementation/src/ObjectTracking.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <sstream>
 #include <opencv2/opencv.hpp>
+#include <thread>
+/*
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/videoio.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/video.hpp>
-
+*/
 
 #include "trajectory_predictor.h"
 
@@ -21,12 +23,26 @@ const char* input_params = "{ input | space_traffic.mp4 | Define the full input 
 
 
 
-int main(int argc, char* argv[])
+int main()
 {
-    CommandLineParser parser(argc, argv, input_params);
+    //CommandLineParser parser(argc, argv, input_params);
 
-    TrajectoryPredictor cap1 = TrajectoryPredictor(0);
-    cap1.getMovingObjects();
+    std::thread t1([&]() {
+        TrajectoryPredictor cap1 = TrajectoryPredictor(0);
+        cap1.getMovingObjects();
+        });
+
+    std::thread t2([&]() {
+        TrajectoryPredictor cap2 = TrajectoryPredictor(1);
+        cap2.getMovingObjects();
+        });
+    
+    //cap1.getMovingObjects();
+    cout << "Cam1 \n";
+    t1.join();
+    //cap2.getMovingObjects();
+    cout << "Cam2 \n";
+    t2.join();
 
     cout << "Done";
     return 0;
